39f79843bbc68fed5c419c7f83fe2f554fe199f0
M1Z43l <mizaelarthur2001@gmail.com>
2023-07-28 01:20:53 -0300

    Python

--- Python/exemplo_database.py
+++ Python/exemplo_database.py
@@ -38,8 +38,8 @@ def inserirDados():
     
     for setorSiape in setSetorSiape:
         print(setorSiape)
-        if not setorSiape: setorSiape = '------'
-        retorno = insereSiape(setorSiape, connDB)
+        if not setorSiape: setorSiape = 'Nenhum'
+        retorno = insertSiape(setorSiape, connDB)
         if not retorno[0]:
             print(retorno[1])
             continue
@@ -50,8 +50,8 @@ def inserirDados():
     print('\nInserindo dados na tabela CARGO')
     dictCargo = dict()
     for cargo in setCargo:
-        if not cargo: cargo = '------'
-        retorno = insereCargo(cargo, connDB)
+        if not cargo: cargo = 'Nenhum'
+        retorno = insertCargo(cargo, connDB)
         if not retorno[0]:
             print(retorno[1])
             continue
@@ -61,8 +61,8 @@ def inserirDados():
     print('\nInserindo dados na tabela SETOR_SIAPE')
     dictSetorSiape = dict()
     for setorSiape in setSetorSiape:
-        if not setorSiape: setorSiape = '------'
-        retorno = insereSiape(setorSiape, connDB)
+        if not setorSiape: setorSiape = 'Nenhum'
+        retorno = insertSiape(setorSiape, connDB)
         if not retorno[0]:
             print(retorno[1])
             continue
@@ -72,8 +72,8 @@ def inserirDados():
     print('\nInserindo dados na tabela DISCIPLINA_INGRESSO')
     dictDisciplinaIngresso = dict()
     for disciplinaIngresso in setDisciplinaIngresso:
-        if not disciplinaIngresso: disciplinaIngresso = '------'
-        retorno = insereDisciplina(disciplinaIngresso, connDB)
+        if not disciplinaIngresso: disciplinaIngresso = 'Nenhum'
+        retorno = insertDisciplina(disciplinaIngresso, connDB)
         if not retorno[0]:
             print(retorno[1])
             continue
@@ -82,8 +82,8 @@ def inserirDados():
     print('\nInserindo dados na tabela CATEGORIA')
     dictCategoria = dict()
     for categoria in setCategoria:
-        if not categoria: categoria = '------'
-        retorno = insereCategoria(categoria, connDB)
+        if not categoria: categoria = 'Nenhum'
+        retorno = insertCategoria(categoria, connDB)
         if not retorno[0]:
             print(retorno[1])
             continue
@@ -93,8 +93,8 @@ def inserirDados():
     print('\nInserindo dados na tabela SETOR_SUAP')
     dictSetorSuap = dict()
     for setorSuap in setSetorSuap:
-        if not setorSuap: setorSuap = '------'
-        retorno = insereSuap(setorSuap, connDB)
+        if not setorSuap: setorSuap = 'Nenhum'
+        retorno = insertSuap(setorSuap, connDB)
         if not retorno[0]:
             print(retorno[1])
             continue
@@ -103,8 +103,8 @@ def inserirDados():
     print('\nInserindo dados na tabela FUNCAO')
     dictFuncao = dict()
     for funcao in setFuncao:
-        if not funcao: funcao  = '------'
-        retorno = insereFuncao(funcao, connDB)
+        if not funcao: funcao  = 'Nenhum'
+        retorno = insertFuncao(funcao, connDB)
         if not retorno[0]:
             print(retorno[1])
             continue
@@ -113,8 +113,8 @@ def inserirDados():
     print('\nInserindo dados na tabela JORNADA_TRABALHO')
     dictJornadaTrabalho = dict()
     for jornadaTrabalho in setJornadaTrabalho:
-        if not jornadaTrabalho: jornadaTrabalho = '------'
-        retorno = insereJornada(jornadaTrabalho, connDB)
+        if not jornadaTrabalho: jornadaTrabalho = 'Nenhum'
+        retorno = insertJornada(jornadaTrabalho, connDB)
         if not retorno[0]:
             print(retorno[1])
             continue
@@ -123,8 +123,8 @@ def inserirDados():
     print('\nInserindo dados na tabela CAMPUS')
     dictCampus = dict()
     for campus in setCampus:
-        if not campus: campus = '------'
-        retorno = insereCampus(campus, connDB)
+        if not campus: campus = 'Nenhum'
+        retorno = insertCampus(campus, connDB)
         if not retorno[0]:
             print(retorno[1])
             continue
@@ -134,22 +134,22 @@ def inserirDados():
     
     #Insere os dados na tabela SERVIDOR
     print('\nInserindo dados na tabela SERVIDOR')
-    tupleCampos = tuple(['categoria'         ,   'cargo'      ,  'setor_siape'       ,
-                        'disciplina'        ,   'setor_suap' ,  'nome'           ,
-                        'funcao'            ,   'jornada_trabalho'    ,  'telefones'   ,
-                        'matricula'   ,   'curriculo_lattes'     ,  'campus'      ,   
-                        'url_fotos'])
+    tupleCampos = tuple(['id_categoria'         ,   'id_cargo'      ,  'id_setor'       ,
+                        'id_disciplina'        ,   'id_setor_suap' ,  'nome'           ,
+                        'id_funcao'            ,   'id_jornada'    ,  'telefones'   ,
+                        'matricula'            ,   'lattes'     ,  'id_campus'      ,   
+                        'url_foto'])
 
 
     for k,v in dados_lidos.items():
-        if dados_lidos[k]['categoria']                  == '': dados_lidos[k]['categoria']                  = '------'
-        if dados_lidos[k]['cargo']                      == '': dados_lidos[k]['cargo']                      = '------'
-        if dados_lidos[k]['setor_siape']                == '': dados_lidos[k]['setor_siape']                = '------'
-        if dados_lidos[k]['disciplina_ingresso']        == '': dados_lidos[k]['disciplina_ingresso']        = '------'
-        if dados_lidos[k]['setor_suap']                 == '': dados_lidos[k]['setor_suap']                 = '------'
-        if dados_lidos[k]['funcao']                     == '': dados_lidos[k]['funcao']                     = '------'
-        if dados_lidos[k]['jornada_trabalho']           == '': dados_lidos[k]['jornada_trabalho']           = '------'
-        if dados_lidos[k]['campus']                     == '': dados_lidos[k]['campus']                     = '------'
+        if dados_lidos[k]['categoria']                  == '': dados_lidos[k]['categoria']                  = 'Nenhum'
+        if dados_lidos[k]['cargo']                      == '': dados_lidos[k]['cargo']                      = 'Nenhum'
+        if dados_lidos[k]['setor_siape']                == '': dados_lidos[k]['setor_siape']                = 'Nenhum'
+        if dados_lidos[k]['disciplina_ingresso']        == '': dados_lidos[k]['disciplina_ingresso']        = 'Nenhum'
+        if dados_lidos[k]['setor_suap']                 == '': dados_lidos[k]['setor_suap']                 = 'Nenhum'
+        if dados_lidos[k]['funcao']                     == '': dados_lidos[k]['funcao']                     = 'Nenhum'
+        if dados_lidos[k]['jornada_trabalho']           == '': dados_lidos[k]['jornada_trabalho']           = 'Nenhum'
+        if dados_lidos[k]['campus']                     == '': dados_lidos[k]['campus']                     = 'Nenhum'
 
         dados_lidos[k]['categoria']                  = dictCategoria[dados_lidos[k]['categoria']]
         dados_lidos[k]['cargo']                      = dictCargo[dados_lidos[k]['cargo']]
@@ -163,7 +163,7 @@ def inserirDados():
 
         tupleValores = tuple(v.values())
 
-        retorno = insereServidor(tupleCampos, tupleValores, connDB)
+        retorno = insertServidor(tupleCampos, tupleValores, connDB)
 
 
     if not retorno[0]: print(retorno[1])

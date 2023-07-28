d0bfea24392ddb4f357d9277cdad90e00353e61f
M1Z43L <mizaelarthur2001@gmail.com>
2023-07-27 21:47:52 -0300

    Update

--- Python/constantes.py
+++ Python/constantes.py
@@ -1,7 +1,10 @@
 import os
 
+# Variavel para armazenar o caminho do diretório
 APP_DIR = os.path.dirname(os.path.abspath(__file__))
 
+# Definindo Caracteres para Portugues Brasil
 CODE_PAGE = "utf-8"
 
+# Aqui informamos qual será o separador das colunas do nosso arquivo
 SEPARATOR = ";"

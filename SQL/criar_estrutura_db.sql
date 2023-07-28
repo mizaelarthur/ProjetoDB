8585f9ecc8d9c23dd02974d71db68b3917b2faac
M1Z43l <mizaelarthur2001@gmail.com>
2023-07-28 00:11:40 -0300

    SQL

--- SQL/criar_estrutura_db.sql
+++ SQL/criar_estrutura_db.sql
@@ -4,3 +4,13 @@ CREATE TABLE cargo(
     CONSTRAINT pk_id_cargo PRIMARY KEY (id_cargo),
     CONSTRAINT un_cargo UNIQUE(cargo)
 );
+
+
+CREATE TABLE funcao(
+    id_funcao   SERIAL,
+    funcao VARCHAR(255) NOT NULL,
+    CONSTRAINT pk_id_funcao PRIMARY KEY (id_funcao),
+    CONSTRAINT un_funcao UNIQUE(funcao)
+);
+
+

5a427b7deeb533c29e1291d7e0861e71427de372
M1Z43l <mizaelarthur2001@gmail.com>
2023-07-28 00:30:20 -0300

    SQL

--- SQL/consultas.sql
+++ SQL/consultas.sql
@@ -2,3 +2,15 @@
 
 
 -- Criando a Primeira View
+CREATE VIEW servidorescampus AS
+	SELECT  campus.campus as sigla,
+			categoria.categoria as categoria,
+	 COUNT	(servidor.matricula) AS nº_servidores
+	  FROM	servidor
+INNER JOIN	campus ON servidor.id_campus = campus.id_campus
+INNER JOIN	categoria ON servidor.id_categoria = categoria.id_categoria
+GROUP BY	campus.campus,
+			categoria.categoria;
+SELECT * FROM servidorescampus ORDER BY sigla;
+
+

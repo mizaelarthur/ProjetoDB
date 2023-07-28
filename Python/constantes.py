d1dc444626e14df208bdb92e592902d21977b1c7
M1Z43l <mizaelarthur2001@gmail.com>
2023-07-28 04:32:53 -0300

    update

--- Python/constantes.py
+++ Python/constantes.py
@@ -1,7 +1,10 @@
+# Importando a Biblioteca "os" para ajudar a obter o caminho do nosso diretório
 import os
 
 APP_DIR = os.path.dirname(os.path.abspath(__file__))
 
+# Tipo de codificação de linguagem para Portugues Brasil
 CODE_PAGE = "utf-8"
 
+# Definindo item separador de colunas no nosso arquivo dados.csv
 SEPARATOR = ";"

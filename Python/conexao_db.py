633b56f7008a18c7974998ab9116050577cae565
M1Z43l <mizaelarthur2001@gmail.com>
2023-07-28 04:31:26 -0300

    update

--- Python/conexao_db.py
+++ Python/conexao_db.py
@@ -1,6 +1,6 @@
-# Constantes de Conexão com o banco
+# Configuração do seu BD
 
-DB_HOST = 'localhost'       
-DB_NAME = 'postgres'     
-DB_USER = 'postgres'        
-DB_PASS = '12345'   
+DB_HOST = 'localhost'   # Endereço do Servidor
+DB_NAME = 'postgres'    # Nome do DB (caso você crie um, basta trocar para o nome do que foi criado)
+DB_USER = 'postgres'    # Usuário para autenticação no DB
+DB_PASS = '12345'       # Senha para autenticação no DB

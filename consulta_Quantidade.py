b808763209532a8fecfa14840e32f4ab80884c3c
M1Z43l <mizaelarthur2001@gmail.com>
2023-07-28 00:50:58 -0300

    Alterações

---
+++ consulta_Quantidade.py
@@ -0,0 +1,13 @@
+import sys
+from Python.configBD import *
+from Python.lib_exemplo import *
+from Python.exemplo_database import *
+from Python.consultas import *
+
+
+def main():
+       
+    DocentesPorDisciplinasECampus()
+
+if __name__ == "__main__":
+    main()
